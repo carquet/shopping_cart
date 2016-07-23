e4c10a178254a1ccdc0e8fe754ff4cd0f21df834
L.Carquet <laetiticarquet@gmail.com>
2016-07-23 15:03:13 +0200

    add functinn price to item without discount and debug conditional in class fruits

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -11,6 +11,9 @@ class Item
 		@name = name
 		@base_price = base_price
 	end	
+	def price
+		price = @base_price
+	end
 end
 
 
@@ -26,7 +29,6 @@ class Houseware < Item
 end
 
 class Fruits < Item
-
 	def price(days)
 		mon = 1
 		tue = 2
@@ -35,7 +37,7 @@ class Fruits < Item
 		fri = 5
 		sat = 6
 		sun = 7
-		if days == 6..7
+		if days == (6..7)
 			puts @base_price +=  (@base_price * 10)/100
 		else
 			puts @base_price
@@ -43,14 +45,29 @@ class Fruits < Item
 	end
 end
 
+
+milk = Item.new("milk", 10)
+puts milk.price
+
+#trying out different hardware with or without discount
 screw = Houseware.new("screw", 120)
 puts screw.price
 
 hammer = Houseware.new("hammer", 80)
 puts hammer.price
 
+#trying different fruits according to the day
 apple = Fruits.new("apple",10)
 puts apple.price(6)
 
 pear = Fruits.new("pear", 25)
 puts pear.price(2)
+
+
+
+
+
+
+
+
+
