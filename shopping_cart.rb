e293c4bd2a15c8ffeaf472c1a6c8b275e39a7672
L.Carquet <laetiticarquet@gmail.com>
2016-07-23 13:55:14 +0200

    calculate the 5% discount if the unit is more than 100euros

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -4,10 +4,30 @@ class ShoppingCart
 	end
 end
 
+
 class Item
-	attr_reader :name, :price
-	def initialize(name, price)
+	attr_reader :name, :base_price
+	def initialize(name, base_price)
 		@name = name
-		@price = price
+		@base_price = base_price
+	end	
+end
+
+
+
+class Houseware < Item
+	def price
+		if @base_price >= 100
+			puts @base_price +=  (@base_price * 5)/100
+		else
+			puts @base_price
+		end
 	end
 end
+
+
+screw = Houseware.new("screw", 120)
+puts screw.price
+
+hammer = Houseware.new("hammer", 80)
+puts hammer.price
