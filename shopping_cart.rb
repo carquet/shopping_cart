5413e33f2b7a6fc7d6c24b6e222477e046afcdf5
L.Carquet <laetiticarquet@gmail.com>
2016-07-26 18:38:01 +0200

    adds item to the shopping cart and calculate total añount to pay

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -45,7 +45,10 @@ class ShoppingCart
 		@item = []
 	end
 
-	
+	def add_item(item)
+		@item.push(item)
+	end
+
 
 	def total_price
 		total_amount_to_pay = 0
@@ -61,9 +64,9 @@ end
 
 #iteration one
 banana = Fruit.new("banana", 10, true)
-puts banana.price
+puts "This is the price for banana at the week end : #{banana.price}"
 apple = Fruit.new("apple", 11, false)
-puts apple.price
+puts "this is the price of apple on week days: #{apple.price}"
 orange_juice = Item.new("Orange Juice", 10)
 puts "the price of orange juice is :#{orange_juice.price} euros"
 rice = Item.new("rice", 1)
@@ -73,7 +76,11 @@ puts "this is the price for a vacuum cleaner: #{vacuum_cleaner.price} euros"
 anchovies = Item.new("anchovies", 2)
 puts "this is the price for anchovies : #{anchovies.price} euros"
 
-
+momma_list = ShoppingCart.new
+momma_list.add_item(banana)
+momma_list.add_item(orange_juice)
+momma_list.add_item(vacuum_cleaner)
+print "This is the total price of your cart: #{momma_list.total_price} euros"
 
 
 
